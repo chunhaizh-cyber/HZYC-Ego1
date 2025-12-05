faa80633349c14ce995c0f1c3187205477f5d6df
root <root@inss-iprj6931e5f266cd5b2375ec1c99.inss-iprj6931e5f266cd5b2375ec1c99.inscode.svc.cluster.local>
2025-12-05 11:30:21 +0800

    Auto Commit 2025-12-05T03:30:21.114Z

---
+++ CMakeFiles/DigitalLifeVisionSystem.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/6c3cY245tiJWEqTvSjPl/ExistenceFeature.cpp" "/root/6c3cY245tiJWEqTvSjPl/CMakeFiles/DigitalLifeVisionSystem.dir/ExistenceFeature.cpp.o"
+  "/root/6c3cY245tiJWEqTvSjPl/PointCloudProcessor.cpp" "/root/6c3cY245tiJWEqTvSjPl/CMakeFiles/DigitalLifeVisionSystem.dir/PointCloudProcessor.cpp.o"
+  "/root/6c3cY245tiJWEqTvSjPl/Vector3D.cpp" "/root/6c3cY245tiJWEqTvSjPl/CMakeFiles/DigitalLifeVisionSystem.dir/Vector3D.cpp.o"
+  "/root/6c3cY245tiJWEqTvSjPl/main.cpp" "/root/6c3cY245tiJWEqTvSjPl/CMakeFiles/DigitalLifeVisionSystem.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/DigitalLifeVisionSystem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DigitalLifeVisionSystem.dir/ExistenceFeature.cpp.o"
+  "CMakeFiles/DigitalLifeVisionSystem.dir/PointCloudProcessor.cpp.o"
+  "CMakeFiles/DigitalLifeVisionSystem.dir/Vector3D.cpp.o"
+  "CMakeFiles/DigitalLifeVisionSystem.dir/main.cpp.o"
+  "bin/DigitalLifeVisionSystem"
+  "bin/DigitalLifeVisionSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DigitalLifeVisionSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
